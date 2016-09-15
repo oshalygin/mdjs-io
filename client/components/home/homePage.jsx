6fd0f3dc14d0b6ac46621eb249b5de4dd4fb8f31
Oleg Shalygin <oshalygin@gmail.com>
2016-09-15 00:49:05 -0700

    cleaning out linting errors and adding the linter to the CI process

--- client/components/home/homePage.jsx
+++ client/components/home/homePage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import React from "react";
-import { Link } from "react-router";
+// import { Link } from "react-router";
 
 class HomePage extends React.Component {
     render() {
--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -3,10 +3,6 @@ import initialState from "./initialState";
 
 export default function itemReducer(state = initialState.user, action) {
     switch (action.type) {
-        case actionTypes.LOGIN_REQUEST: {
-
-            break;
-        }
         case actionTypes.LOGIN_SUCCESS: {
             return {...action.user.user, token: action.user.token };
         }
