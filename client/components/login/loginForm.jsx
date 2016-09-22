dc35b812402fd5ea1257f59a85db8f223e0b30ee
Oleg Shalygin <oshalygin@gmail.com>
2016-09-21 21:14:22 -0700

    formatting of loginForm post TS update

--- client/components/login/loginForm.jsx
+++ client/components/login/loginForm.jsx
@@ -2,35 +2,35 @@ import React, { PropTypes } from "react";
 
 const LoginForm = ({onChange, login}) => {
     return (
-                    <form className="m-t">
-                        <div className="form-group">
-                            <input
-                                type = "email"
-                                name = "email"
-                                className = "form-control"
-                                placeholder = "Username"
-                                onChange = {onChange}
-                                required />
-                        </div>
-                    <div className="form-group">
-                            <input
-                                type = "password"
-                                name = "password"
-                                className = "form-control"
-                                placeholder = "Password"
-                                onChange = {onChange}
-                                required />
-                        </div>
-                        <button
-                            type = "submit"
-                            className = "btn btn-primary block full-width m-b"
-                            onClick = {login}
-                            onSubmit = {login}>Login</button>
+        <form className="m-t">
+            <div className="form-group">
+                <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="Username"
+                    onChange={onChange}
+                    required />
+            </div>
+            <div className="form-group">
+                <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Password"
+                    onChange={onChange}
+                    required />
+            </div>
+            <button
+                type="submit"
+                className="btn btn-primary block full-width m-b"
+                onClick={login}
+                onSubmit={login}>Login</button>
 
-                        <a href="#"><small>Forgot password?</small></a>
-                        <p className="text-muted text-center"><small>Don't have an account?</small></p>
-                        <a className="btn btn-sm btn-white btn-block" href="#">Create an account</a>
-                    </form>
+            <a href="#"><small>Forgot password?</small></a>
+            <p className="text-muted text-center"><small>Don't have an account?</small></p>
+            <a className="btn btn-sm btn-white btn-block" href="#">Create an account</a>
+        </form>
     );
 };
 
