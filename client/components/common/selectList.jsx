aa25e28eb3063f979b81fab010e96908fb4f5319
Oleg Shalygin <oshalygin@gmail.com>
2016-09-23 19:06:29 -0700

    implementation of selectListRow pulling from constants, still needs to handle null selection, #26

--- client/components/common/selectList.jsx
+++ client/components/common/selectList.jsx
@@ -1,10 +1,11 @@
 import React, { PropTypes } from "react";
+import SelectListRow from "./selectListRow.jsx";
 
-const SelectList = ({value, label, name, options, defaultOption, onChange}) => {
+const SelectList = ({value, label, name, options, onChange}) => {
     let classList = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select";
     if (!!value) {
         classList += " is-dirty";
-   }
+    }
     return (
         <div className={classList} >
             <input className="mdl-textfield__input"
@@ -17,9 +18,14 @@ const SelectList = ({value, label, name, options, defaultOption, onChange}) => {
                 data-value={value} />
             <label htmlFor={name} className="mdl-textfield__label">{label}</label>
             <ul htmlFor={name} className="mdl-menu mdl-menu--bottom-left mdl-js-menu" name="priceTypeId">
-                <li className="mdl-menu__item" name={name} data-value="0" onClick={onChange}>Each</li>
-                <li className="mdl-menu__item" name={name} data-value="1" onClick={onChange}>lb</li>
-                <li className="mdl-menu__item" name={name} data-value="2" onClick={onChange}>oz</li>
+                {options.map(option => {
+                    return (<SelectListRow
+                        key={option.value}
+                        label={option.label}
+                        name={name}
+                        value={option.value}
+                        onChange={onChange} />);
+                })}
             </ul>
         </div>
     );
--- client/components/common/selectListRow.jsx
+++ client/components/common/selectListRow.jsx
@@ -1,1 +1,16 @@
-// import React, { PropTypes } from "react";
+import React, { PropTypes } from "react";
+
+const SelectListRow = ({name, label, value, onChange}) => {
+    return (
+        <li className="mdl-menu__item" name={name} data-value={value} onClick={onChange}>{label}</li>
+    );
+};
+
+SelectListRow.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.any.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+
+export default SelectListRow;
