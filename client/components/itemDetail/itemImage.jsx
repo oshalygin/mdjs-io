6f23116277daac23b41c7c34a81b5fe9ed0f977a
Oleg Shalygin <oshalygin@gmail.com>
2016-09-28 22:27:08 -0700

    wiring up file dropzone

--- client/components/itemDetail/itemImage.jsx
+++ client/components/itemDetail/itemImage.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from "react";
+import DropZone from "react-dropzone";
 
 const imageBoxLength = 10;
 const unitOfMeasure = "em";
@@ -12,7 +13,7 @@ const imagePlaceholderContainer = {
 };
 const imageContainer = {
     position: "absolute",
-    margin: `-${imageBoxLength/2}${unitOfMeasure} auto`,
+    margin: `-${imageBoxLength / 2}${unitOfMeasure} auto`,
     width: "100%",
     height: `${imageBoxLength}${unitOfMeasure}`,
     top: "50%"
@@ -22,18 +23,31 @@ const imageItem = {
     borderRadius: "1.5em",
     height: `${imageBoxLength}${unitOfMeasure}`,
     width: `${imageBoxLength}${unitOfMeasure}`,
-    margin: "0 auto"
+    margin: "0 auto",
+    textAlign: "center",
+    verticalAlign: "middle",
+    lineHeight: `${imageBoxLength - imageBoxLength/10}${unitOfMeasure}`
 };
+
+function readFiles(files) {
+    console.log(files);
+}
 
 const ItemImage = () => {
     return (
         <div style={imagePlaceholderContainer}>
-            <div style={imageContainer}>
-                <div style={imageItem} />
-            </div>
+            <DropZone style={imageContainer} onDrop={(files) => readFiles(files)}>
+                <div style={imageItem}>Drag/Click Here</div>
+            </DropZone>
         </div>
     );
 };
+
+// <div style={imagePlaceholderContainer}>
+//             <div style={imageContainer}>
+//                 <div style={imageItem} />
+//             </div>
+//         </div>
 
 ItemImage.propTypes = {
 
